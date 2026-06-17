55f6dc526e5a9537da6d8c9a4803a210200f557b
Nonn <apisityambangyang@gmail.com>
2026-06-17 10:14:29 +0700

    getDatabase

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,3 +20,7 @@ Route::get('/2', function () {
 });
 
 Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/machines', function () {
+    return DB::table('machines')->get();
+});
