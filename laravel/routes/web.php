43efde4a78387f8d2e55fc9bc86ca6a480cec9a3
Nonn <apisityambangyang@gmail.com>
2026-06-17 09:25:12 +0700

    JsonNJ

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/1', function () {
+    return 'Hello Test 123
+    ';
+});
+
+Route::get('/2', function () {
+    return ['name' => 'Apisit'
+          ,'job' => 'programmer'
+    ];
+});
+
+Route::get('/user', [UserController::class, 'index']);
